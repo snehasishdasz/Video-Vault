07dfa55adb506778782c3f130bd1d3ffe917e165
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2023-07-30 12:12:41 +0530

    footer-done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,49 @@
+import { Box, Button, HStack, Heading, Input, Stack, Text, VStack } from '@chakra-ui/react'
+import React from 'react'
+import {AiFillInstagram, AiFillTwitterSquare, AiFillYoutube, AiOutlineSend} from "react-icons/ai"
+
+const Footer = () => {
+  return (
+    <Box bgColor={"black"} minH={"40"} p={"16"} color={"white"} >
+        <Stack direction={["column","row"]}>
+            <VStack alignItems={"stretch"} w={'full'} px={"4"}>
+                <Heading size={"md"} textTransform={"uppercase"} textAlign={["center","left"]}>
+                    Subscribe to get updates
+                </Heading>
+                <HStack borderBottom={"2px solid white"} py={"2"}>
+                <Input placeholder='Enter email here...' border={"none"} borderRadius={"none"} outline={"none"} focusBorderColor='none'/>
+                    <Button p={"0"} color={"white"} bgColor={"red"} borderRadius={"0 20px 0 0"}>
+                        <AiOutlineSend size={"20"}/>
+                    </Button>
+                </HStack>
+            </VStack>
+            <VStack w={'full'} 
+            borderLeft={["none","1px solid white"]}
+            borderRight={["none","1px solid white"]}
+            >
+                <Heading  textAlign={"center"}><span style={{color:"#FF2E63",fontSize:"45px"}}>V</span>ideo<span style={{color:"#FF2E63",fontSize:"45px"}}>V</span>ault</Heading>
+                <Text color={"white"} pt={"4"}>Created with ❤️ by Abhishek & Snehasish</Text>
+            </VStack>
+            <VStack w={'full'}>
+                <Heading size={"md"} textTransform={"uppercase"} pb={"5px"}>Social Media</Heading>
+                <HStack>
+                <Button color='white'  >
+                    <AiFillYoutube size={'30px'}/>
+                    
+                </Button>
+                <Button color='white'  >
+                    <AiFillInstagram size={'30px'}/>
+                </Button>
+                <Button color='white'  >
+                    <AiFillTwitterSquare size={'30px'}/>
+                </Button>
+                </HStack>
+                
+            </VStack>
+        </Stack>
+
+    </Box>
+  )
+}
+
+export default Footer
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
-import {Carousel} from "react-responsive-carousel";
-import  "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import img1 from "../assests/N2.jpg"
 import img2 from "../assests/A4.jpg"
 import img3 from "../assests/C2.jpg"
@@ -37,71 +37,71 @@ const headingOptions = {
     top: "50%",
     transform: "translate(-50%, -50%)",
     textTransform: "uppercase",
-    p:"4",
+    p: "4",
     size: "4xl"
 }
 
 const Home = () => {
-    
-  return (
-    <Box style={{textAlign:"center"}}w={'400'}>
-        {/* <MyCarousel/> */}
-        <Carousel 
-    autoPlay 
-    infiniteLoop 
-    interval={1000} 
-    showStatus={false} 
-    showThumbs={false} 
-    showArrows={false}>
 
-        <Box  h={"100vh"}>
-            <img   style={{borderRadius:"120px",padding:"80px"}}  src={img1}/>
-            <Heading bgColor={"white"} color={"black"} pos={"absolute"} {...headingOptions}>Nature</Heading>
-        </Box>
-        <Box w={'full'} h={"100vh"}>
-            <Image w={"400"} p={"80px"} src={img2}/>
-            <Heading bgColor={"white"} color={"black"} {...headingOptions}>Animal</Heading>
-        </Box>
-        <Box w={'full'} h={"100vh"}>
-            <Image w={"400"} p={"80px"} src={img3}/>
-            <Heading bgColor={"white"} color={"black"} {...headingOptions}>Games</Heading>
-        </Box>
-        <Box w={'full'} h={"100vh"}>
-            <Image w={"400"} p={"80px"} src={img4}/>
-            <Heading bgColor={"white"} color={"black"} {...headingOptions}>Racing</Heading>
-        </Box>
-        <Box w={'full'} h={"100vh"}>
-            <Image w={"400"} p={"80px"}  src={img5}/>
-            <Heading bgColor={"white"} color={"black"} {...headingOptions}>Sports</Heading>
-        </Box>
-        <Box w={'full'} h={"100vh"}>
-            <Image w={"400"} p={"80px"} src={img6}/>
-            <Heading bgColor={"white"} color={"black"} {...headingOptions}>Gaming</Heading>
-        </Box>
-    </Carousel>
-    <Container maxWidth={"container.xl"} minH={"70vh"} p={"16"} >
-        <Heading textTransform={"uppercase"} py={"2"} w={"fit-content"} borderBottom={"2px solid"} m={"auto"} ><span style={{color:"#FF2E63",fontSize:"50px"}}>A</span>bout Us</Heading>
+    return (
+        <Box style={{ textAlign: "center" }} w={'400'}>
+            {/* <MyCarousel/> */}
+            <Carousel
+                autoPlay
+                infiniteLoop
+                interval={1000}
+                showStatus={false}
+                showThumbs={false}
+                showArrows={false}>
 
-        <Stack
-        h={"full"}
-        p={"4"}
-        alignItems={"center"}
-        direction={["column", "row"]}
-        >
+                <Box h={"100vh"}>
+                    <img style={{ borderRadius: "120px", padding: "80px" }} src={img1} />
+                    <Heading bgColor={"white"} color={"black"} pos={"absolute"} {...headingOptions}>Nature</Heading>
+                </Box>
+                <Box w={'full'} h={"100vh"}>
+                    <img style={{ borderRadius: "120px", padding: "80px" }} src={img2} />
+                    <Heading bgColor={"white"} color={"black"} {...headingOptions}>Animal</Heading>
+                </Box>
+                <Box w={'full'} h={"100vh"}>
+                    <img style={{ borderRadius: "120px", padding: "80px" }} src={img3} />
+                    <Heading bgColor={"white"} color={"black"} {...headingOptions}>Games</Heading>
+                </Box>
+                <Box w={'full'} h={"100vh"}>
+                    <img style={{ borderRadius: "120px", padding: "80px" }} src={img4} />
+                    <Heading bgColor={"white"} color={"black"} {...headingOptions}>Racing</Heading>
+                </Box>
+                <Box w={'full'} h={"100vh"}>
+                    <img style={{ borderRadius: "120px", padding: "80px" }} src={img5} />
+                    <Heading bgColor={"white"} color={"black"} {...headingOptions}>Sports</Heading>
+                </Box>
+                <Box w={'full'} h={"100vh"}>
+                    <img style={{ borderRadius: "120px", padding: "80px" }} src={img6} />
+                    <Heading bgColor={"white"} color={"black"} {...headingOptions}>Gaming</Heading>
+                </Box>
+            </Carousel>
+            <Container maxWidth={"container.xl"} minH={"70vh"} p={"16"} >
+                <Heading textTransform={"uppercase"} py={"2"} w={"fit-content"} borderBottom={"2px solid"} m={"auto"} ><span style={{ color: "#FF2E63", fontSize: "50px" }}>A</span>bout Us</Heading>
 
-        <Image src={img7} h={["30","300"]}/>
-        <Text letterSpacing={"widest"} lineHeight={"190%"} p={["4","16"]} fontFamily={"Montserrat"} textAlign={"center"} fontWeight={"700"}>
-            
-Welcome to our video streaming application, your gateway to a diverse world of entertainment! With a user-friendly interface and advanced search, you'll find the perfect content tailored to your preferences. Enjoy high-definition nature documentaries, where you can explore exotic landscapes and wildlife in stunning detail. Delight in heartwarming animal moments, from adorable pets to majestic creatures, captivating you with their charm and intelligence.
+                <Stack
+                    h={"full"}
+                    p={"4"}
+                    alignItems={"center"}
+                    direction={["column", "row"]}
+                >
 
-For sports enthusiasts, we offer real-time events, matches, and highlights from popular leagues and niche competitions. Stay up to date with the latest gaming trends, walkthroughs, reviews, and exciting live streams from your favorite gamers. Experience the adrenaline rush with our collection of jaw-dropping racing videos, featuring epic races and thrilling stunts across various terrains.
+                    <Image src={img7} h={["40", "300"]} />
+                    <Text letterSpacing={"widest"} lineHeight={"190%"} p={["4", "16"]} fontFamily={"Montserrat"} textAlign={"center"} fontWeight={"700"}>
 
-        </Text>
-        </Stack>
-    </Container>
-        
-    </Box>
-  )
+                        Welcome to our <span style={{ color: "#FF2E63", fontSize: "30px" }}>V</span>IDEO <span style={{ color: "#FF2E63", fontSize: "30px" }}>S</span>treaming application, your gateway to a diverse world of entertainment! With a user-friendly interface and advanced search, you'll find the perfect content tailored to your preferences. Enjoy high-definition nature documentaries, where you can explore exotic landscapes and wildlife in stunning detail. Delight in heartwarming animal moments, from adorable pets to majestic creatures, captivating you with their charm and intelligence.
+
+                        For sports enthusiasts, we offer real-time events, matches, and highlights from popular leagues and niche competitions. Stay up to date with the latest gaming trends, walkthroughs, reviews, and exciting live streams from your favorite gamers. Experience the adrenaline rush with our collection of jaw-dropping racing videos, featuring epic races and thrilling stunts across various terrains.
+
+                    </Text>
+                </Stack>
+            </Container>
+
+        </Box>
+    )
 };
 
 
